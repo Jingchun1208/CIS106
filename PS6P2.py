f6eee719805611cf549b4a754e554db19e808470
Jingchun1208 <jyan33@illinois.edu>
2025-01-30 23:41:15 -0600

    Create PS6P2.py

---
+++ PS6P2.py
@@ -0,0 +1,11 @@
+#input phase
+start_value = int(input("Enter start value"))
+stop_value = int(input("Enter stop value"))
+increment_value = int(input("Enter increment value"))
+
+#process phase
+value = start_value
+while value <= stop_value:
+      print(value)
+      value = value + increment_value
+
