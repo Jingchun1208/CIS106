7e541e439f19b56ebc4e9d1ad5804bfea7432a50
Jingchun1208 <jyan33@illinois.edu>
2025-02-28 00:33:16 -0600

    Create PS13P1(Set11).py

---
+++ PS13P1(Set11).py
@@ -0,0 +1,11 @@
+last_name = ["Albert","White","Knight","Dean","Brown","George","Jackson","Smith","Taylor","Jacky"]
+def display(last_name):
+  for i in range (0,10,1):
+    print(last_name[i])
+def reverse(last_name):
+  for x in range (9,-1,-1):
+    print(last_name[x])
+
+display(last_name)
+print("\n")
+reverse(last_name)
